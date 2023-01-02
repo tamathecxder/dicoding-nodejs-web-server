a91928440b1d53bd68ba3f741b419a1e8d3b2023
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-02 18:41:48 +0700

    Membuat web server sederhana

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const http = require('http');
+
+/**
+ * Logikan untuk menangani dan menganggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: object yang berisikan informasi terkait permintaan
+ * @param response: object yang digunakan untuk menanggapi permintaan 
+ */
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  response.statusCode = 200;
+  response.end('<h1>Halo, HTTP Server!</h1>');
+}
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
