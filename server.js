f1fa081cab71dbb77e1f77135612d1999303cf41
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-02 18:48:00 +0700

    Manipulasi response sesuai dengan method request nya

--- server.js
+++ server.js
@@ -9,9 +9,25 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-
   response.statusCode = 200;
-  response.end('<h1>Halo, HTTP Server!</h1>');
+  
+  const { method } = request;
+
+  if (method == 'GET') {
+    response.end("<h1>Kumaha Damang?</h1>");
+  }
+
+  if (method == 'POST') {
+    response.end("<h1>Apa Kabar?</h1>");
+  }
+
+  if (method == 'PUT') {
+    response.end("<h1>Piye Kabare?</h1>");
+  }
+
+  if (method == 'DELETE') {
+    response.end("<h1>How Are You?</h1>");
+  }
 }
 
 const server = http.createServer(requestListener);
