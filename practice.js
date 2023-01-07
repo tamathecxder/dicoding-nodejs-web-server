65f2b9d6fe93e970246d53bf3a6c6d39d3feba75
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-07 16:08:15 +0700

    Add status code

--- practice.js
+++ practice.js
@@ -2,20 +2,22 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-  response.statusCode = 200;
   
   const { method, url } = request;
   let body = [];
 
   if (url === '/') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end("<h1>Ini adalah homepage</h1>");
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
   } else if (url === '/about') {
     if (method === 'GET') {
       response.end("<h1>Halo! ini adalah halaman about</h1>");
+      response.statusCode = 200;
     } else if (method === 'POST') {
       request.on('data', (chunk) => {
         body.push(chunk);
@@ -25,12 +27,15 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
 
+        response.statusCode = 200;
         response.end(`<h1>Halo, ${name}! ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
   } else {
+    response.statusCode = 404;
     response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 }
